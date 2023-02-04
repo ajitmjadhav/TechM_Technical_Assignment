967276d796a4dc0638b144c305f0c752a746fd1e
Ajit Jadhav <aj00807287@techmahindra.com>
2023-02-04 21:26:51 +0530

    refactor createCurrentDate function and add current time, also update HTML , css for listItems and buttons

--- assignment4-todo_list_manager/src/components/Todolist.js
+++ assignment4-todo_list_manager/src/components/Todolist.js
@@ -26,10 +26,15 @@ const List = () => {
         const yyyy = today.getFullYear();
         let mm = today.getMonth() + 1;
         let dd = today.getDate();
-        // let time = today.getTime();
+        // let time = today.getHours() + ':' + today.getMinutes();
+        let time;
+        let hour = today.getHours();
+        let min = today.getMinutes();
+        if (hour < 12) time = `${hour % 12}:${min} AM`;
+        else { time = `${hour % 12 || 12}:${min} PM` }
         if (dd < 10) dd = '0' + dd;
         if (mm < 10) mm = '0' + mm;
-        const formattedToday = `${dd} / ${mm} / ${yyyy}`;
+        const formattedToday = `${dd} / ${mm} / ${yyyy} ::${time}`;
         return formattedToday;
     }
     // add the Item to the list
@@ -207,7 +212,7 @@ const List = () => {
                         <input type="text" value={inputData} ref={inputRef} placeholder='Enter to do item' onChange={(e) => setInputData(e.target.value)} />
                         {isSubmit
                             ?
-                            <button onClick={addItem} className='add-item-btn form-btn' type='button'>Add</button>
+                            <button onClick={addItem} title='add item' className='add-item-btn form-btn' type='button'>Add</button>
                             :
                             <button onClick={addItem} className='edit-item-btn form-btn' type='button'>Edit</button>
                         }
@@ -223,10 +228,10 @@ const List = () => {
                                         <p className='listItem-date'>{ele.date}</p>
                                     </div>
                                     <div>
-                                        <svg onClick={() => deleteCurrentItem(ele.id)} className='delete-btn svg-btn' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                                        <svg onClick={() => deleteCurrentItem(ele.id)} title='delete item' className='delete-btn svg-btn' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                         </svg>
-                                        <svg onClick={() => editCurrentItem(ele.id)} className='edit-btn svg-btn' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                                        <svg onClick={() => editCurrentItem(ele.id)} title='edit item' className='edit-btn svg-btn' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                                         </svg>
                                     </div>
@@ -235,11 +240,11 @@ const List = () => {
                         })}
                     </div>
                     <div className='delete-all'>
-                        <button className='delete-all-btn' onClick={removeAllItems}>Delete All</button>
+                        <button className='delete-all-btn' title='delete all' onClick={removeAllItems}>Delete All</button>
                     </div>
                     <div className='dynamic-btns'>
-                        <button onClick={loadList} className='load-data-btn load'>Load List</button>
-                        <button onClick={saveList} className='load-data-btn save'>Save All</button>
+                        <button onClick={loadList} title='load old list' className='load-data-btn load'>Load List</button>
+                        <button onClick={saveList} title='save to database' className='load-data-btn save'>Save All</button>
                     </div>
                 </div>
 
