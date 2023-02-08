69daf64d022cb68f1d0e610d2723e8d95448e071
Ajit Jadhav <aj00807287@techmahindra.com>
2023-02-08 14:28:22 +0530

    done with showing fetched data to UI and also done the list and grid view dunctionality

--- assignment1-shopping_app/src/components/home.js
+++ assignment1-shopping_app/src/components/home.js
@@ -3,6 +3,7 @@ import './home.css'
 
 const Home = () => {
     const [dataLoading, setDataLoading] = useState(true);
+    const [isGridView, setIsGridView] = useState(true);
     const [allData, setAllData] = useState([]);
 
     const getWhetherReport = async () => {
@@ -18,6 +19,13 @@ const Home = () => {
         }, 2000);
         return () => clearTimeout(timer);
     }, []);
+
+    const setListView = () => {
+        setIsGridView(false);
+    }
+    const setGridView = () => {
+        setIsGridView(true);
+    }
     return (
         <>
             <div className='wmg-container'>
@@ -25,10 +33,10 @@ const Home = () => {
                     <div className='nav-left'>
                         <h1 className='logo-text'>Shopping Craze</h1>
                         <div className='item-views '>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className=" act-view w-6 h-6">
+                            <svg onClick={setListView} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={isGridView ? '' : 'act-view'}>
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                             </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                            <svg onClick={setGridView} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={isGridView ? 'act-view' : ''}>
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z" />
                             </svg>
 
@@ -51,67 +59,46 @@ const Home = () => {
                     </div>
                 </div>
                 <div className='all-items'>
-                    <div className='item-grid'>
-                        <div className='item-card'>
-                            <div className='card-image'>
-                                <img src='https://images2.imgbox.com/cb/69/cQ29pV1x_o.jpeg' alt='random image from web' />
-                            </div>
-                            <div className='card-detail'>
-                                <h2 className='item-name'>Item Name</h2>
-                                <div>
-                                    <p className='item-cat'>Item Category</p>
-                                    <p className='item-company'>Item company</p>
-                                </div>
-                                <div className='item-desc'>Desc:Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus ante. Nunc euismod risus ut dui interdum ornare. Vestibulum porta ex enim, eget aliquet risus sollicitudin ac.</div>
-                                <h3 className='item-price'>200$</h3>
-                                <button className='add-to-cart'>Add to Cart</button>
-                            </div>
-                        </div>
-                        <div className='item-card'>
-                            <div className='card-image'>
-                                <img src='https://images2.imgbox.com/cb/69/cQ29pV1x_o.jpeg' alt='random image from web' />
-                            </div>
-                            <div className='card-detail'>
-                                <h2 className='item-name'>Item Name</h2>
-                                <div>
-                                    <p className='item-cat'>Item Category</p>
-                                    <p className='item-company'>Item company</p>
-                                </div>
-                                <div className='item-desc'>Desc:Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus ante. Nunc euismod risus ut dui interdum ornare. Vestibulum porta ex enim, eget aliquet risus sollicitudin ac.</div>
-                                <h3 className='item-price'>200$</h3>
-                                <button className='add-to-cart'>Add to Cart</button>
-                            </div>
-                        </div>
-                        <div className='item-card'>
-                            <div className='card-image'>
-                                <img src='https://images2.imgbox.com/cb/69/cQ29pV1x_o.jpeg' alt='random image from web' />
-                            </div>
-                            <div className='card-detail'>
-                                <h2 className='item-name'>Item Name</h2>
-                                <div>
-                                    <p className='item-cat'>Item Category</p>
-                                    <p className='item-company'>Item company</p>
-                                </div>
-                                <div className='item-desc'>Desc:Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus ante. Nunc euismod risus ut dui interdum ornare. Vestibulum porta ex enim, eget aliquet risus sollicitudin ac.</div>
-                                <h3 className='item-price'>200$</h3>
-                                <button className='add-to-cart'>Add to Cart</button>
-                            </div>
-                        </div>
-                        <div className='item-card'>
-                            <div className='card-image'>
-                                <img src='https://images2.imgbox.com/cb/69/cQ29pV1x_o.jpeg' alt='random image from web' />
-                            </div>
-                            <div className='card-detail'>
-                                <h2 className='item-name'>Item Name</h2>
-                                <div>
-                                    <p className='item-cat'>Item Category</p>
-                                    <p className='item-company'>Item company</p>
-                                </div>
-                                <div className='item-desc'>Desc:Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus ante. Nunc euismod risus ut dui interdum ornare. Vestibulum porta ex enim, eget aliquet risus sollicitudin ac.</div>
-                                <h3 className='item-price'>200$</h3>
-                                <button className='add-to-cart'>Add to Cart</button>
-                            </div>
-                        </div>
+                    <div className={isGridView ? 'item-grid' : 'list-view'} >
+
+                        {allData && allData.map((item, index) => {
+                            {/* const [id, name, image, price, colors, company, description, category] = item; */ }
+
+                            return (
+                                isGridView ?
+                                    <div className='item-card' key={item.id}>
+                                        <div className='card-image'>
+                                            <img src={item.image} alt='random image from web' />
+                                        </div>
+                                        <div className='card-detail'>
+                                            <h2 className='item-name'>{item.name}</h2>
+                                            <div>
+                                                <p className='item-cat'>{item.category}</p>
+                                                <p className='item-company'>{item.company}y</p>
+                                            </div>
+                                            <div className='item-desc'>Desc:{item.description}</div>
+                                            <h3 className='item-price'>{item.price}$</h3>
+                                            <button className='add-to-cart'>Add to Cart</button>
+                                        </div>
+                                    </div>
+                                    :
+                                    <div className='item-card-list' key={item.id}>
+                                        <div className='card-image-list'>
+                                            <img src={item.image} alt='random image from web' />
+                                        </div>
+                                        <div className='card-detail-list'>
+                                            <h2 className='item-name'>{item.name}</h2>
+                                            <div>
+                                                <p className='item-cat'>{item.category}</p>
+                                                <p className='item-company'>{item.company}y</p>
+                                            </div>
+                                            <div className='item-desc'>Desc:{item.description}</div>
+                                            <h3 className='item-price'>{item.price}$</h3>
+                                            <button className='add-to-cart'>Add to Cart</button>
+                                        </div>
+                                    </div>
+                            )
+                        })}
                     </div>
                 </div>
             </div>
